016ad71377eda3999131e8ae13a8f3c1fe71fcfe
iamsurajdc <surajchandgude0304@gmail.com>
2018-12-18 17:24:25 +0530

    Added app-router.module, dashboard component, hero-detail isolated.

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -15,4 +15,9 @@ export class HeroService {
     this.msgService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.msgService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { HeroService } from './hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './../hero';
-  
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,19 +11,13 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   heroes: Hero[] ;
-  selectedHero: Hero;
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes)
-  }
 
   ngOnInit(){
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log(this.selectedHero);
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
   }
 }
